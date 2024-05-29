1047c5ccd8fbd51a237d522b5039f79e91c55ff2
eji_a <156595423+e-aji@users.noreply.github.com>
2024-05-29 15:07:52 +0100

    updated seed data and updated code to create collections

--- utils/seed.js
+++ utils/seed.js
@@ -7,17 +7,39 @@ connection.on('error', (err) => err);
 connection.once('open', async () => {
     console.log('connected');
 
+      // Delete the collections if they exist
+    let thoughtCheck = await connection.db.listCollections({ name: 'thoughts' }).toArray();
+    if (thoughtCheck.length) {
+        await connection.dropCollection('thoughts');
+    }
+    
+    let userCheck = await connection.db.listCollections({ name: 'users' }).toArray();
+    if (userCheck.length) {
+        await connection.dropCollection('users');
+    }
 
+    const users = [];
+    const thoughts = getRandomThoughts(10);
 
+    for (let i = 0; i < 20; i++) {
+        const fullName = getRandomName();
+        const first = fullName.split(' ')[0];
+        const last = fullName.split(' ')[1];
 
+        users.push({
+        first,
+        last,
+        age: Math.floor(Math.random() * (99 - 18 + 1) + 18),
+        });
+    }
 
+    await User.collection.insertMany(users);
+    await Thought.collection.insertMany(thoughts);
 
-
-
-
-
-
-
-
+    // loop through the saved thoughts, for each thought we need to generate a thought response and insert the thought responses
+    console.table(users);
+    console.table(thoughts);
+    console.info('Seeding complete! 🌱');
+    process.exit(0);
 
 });
